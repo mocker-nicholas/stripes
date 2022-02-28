3155af163756fd0d6a1f8d304b7fe2f0412e0e24
Nick Orth <nicholas.r.orth@gmail.com>
2022-02-28 09:40:54 -0600

    Add ejs as views engine for application

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import ejs from "ejs";
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+////////// Connect to the database //////////////
 const connectDb = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/stripeshop");
@@ -12,11 +18,17 @@ const connectDb = async () => {
 };
 connectDb();
 
+///////////// app settings ////////////////////
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//////////// Homepage Route ///////////////
 app.get("/", (req, res) => {
-  res.send("Welcome to stripeshop!");
+  res.render("landing");
   //// Change the store name to stripes!
 });
 
+///////////// Listen for requests //////////////
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
