c3d097b3189e14ac16341db9ce380a91d32060e6
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-01 12:50:32 -0600

    Move universal scripts into their own partial to place at bottom of the body, add util js for navbar dropdown

--- public/js/util.js
+++ public/js/util.js
@@ -1,1 +1,11 @@
 console.log("inside pub util js");
+const shopNav = document.querySelector("#shop");
+const dropdown = document.querySelector(".dropdown");
+
+shopNav.addEventListener("mouseover", () => {
+  shopNav.nextElementSibling.classList.remove("hide");
+});
+
+dropdown.addEventListener("mouseleave", () => {
+  dropdown.classList.add("hide");
+});
