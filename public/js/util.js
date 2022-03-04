7f3c4e1b8f6299dd918e9304283c0ffcf512f267
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-04 14:55:03 -0600

    responsive navbar added to all current pages

--- public/js/util.js
+++ public/js/util.js
@@ -1,4 +1,6 @@
 console.log("inside pub util js");
+
+//////////// Add the dropdown menu on hover for the shop button ////////////////
 const shopNav = document.querySelector("#shop");
 const dropdown = document.querySelector(".dropdown");
 
@@ -8,4 +10,11 @@ shopNav.addEventListener("mouseover", () => {
 
 dropdown.addEventListener("mouseleave", () => {
   dropdown.classList.add("hide");
+});
+
+///////////////////////// Display Mobile Menu on a click /////////////////////////////
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  const smallNav = document.querySelector(".small-nav");
+  smallNav.classList.toggle("hide");
 });
