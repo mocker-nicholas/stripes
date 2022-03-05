858289ab00e53e801435f08dcd2982b5b70c9d92
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-05 08:10:22 -0600

    Add jumplinks section for products on index

--- public/js/util.js
+++ public/js/util.js
@@ -1,17 +1,5 @@
 console.log("inside pub util js");
 
-//////////// Add the dropdown menu on hover for the shop button ////////////////
-const shopNav = document.querySelector("#shop");
-const dropdown = document.querySelector(".dropdown");
-
-shopNav.addEventListener("mouseover", () => {
-  shopNav.nextElementSibling.classList.remove("hide");
-});
-
-dropdown.addEventListener("mouseleave", () => {
-  dropdown.classList.add("hide");
-});
-
 ///////////////////////// Display Mobile Menu on a click /////////////////////////////
 const burger = document.querySelector(".burger");
 burger.addEventListener("click", () => {
