de46d4c2cbcaff7755406b8e5d5fab654705cbc1
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-06 21:08:42 -0600

    Add front end form validation for registering a user

--- public/js/pubregister.js
+++ public/js/pubregister.js
@@ -1,1 +1,37 @@
 console.log("Inside public register");
+
+const showError = (element, message) => {
+  const errorDiv = element.nextElementSibling;
+  element.setAttribute("style", "border: 2px solid red");
+  errorDiv.classList.remove("hide");
+  errorDiv.innerText = `${message}`;
+  return;
+};
+
+const formbtn = document.querySelector("#registerbtn");
+formbtn.addEventListener("click", (e) => {
+  const username = document.querySelector("#username");
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  const usernameRegex = /^[a-zA-Z0-9]+$/;
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  if (!username.value.match(usernameRegex)) {
+    e.preventDefault();
+    let message =
+      "Username must be 9 characters long, can include letters, numbers, and dashes";
+    return showError(username, message);
+  }
+  if (!email.value.match(emailRegex)) {
+    e.preventDefault();
+    let message = "Must be a valid email address";
+    return showError(email, message);
+  }
+  if (!password.value.match(passwordRegex)) {
+    e.preventDefault();
+    let message =
+      "Password must be 8 character with one number and one special character";
+    return showError(password, message);
+  }
+});
