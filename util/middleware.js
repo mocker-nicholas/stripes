74f50752a153154e988040a4fbbff990cf22d8d3
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-12 15:13:56 -0600

    got rid of middleware to convert user update data, changed placeholders to values for user update form

--- util/middleware.js
+++ util/middleware.js
@@ -4,7 +4,6 @@ export const validateUser = (req, res, next) => {
   const { error } = joiUserSchema.validate(req.body);
   if (error) {
     req.flash("error", `${error.message}`);
-    console.log(req);
     return res.redirect(`${req.originalUrl}`);
   }
   return next();
