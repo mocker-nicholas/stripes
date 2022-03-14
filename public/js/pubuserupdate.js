00ee03dfb4344de2d77e6eecd064365b0248330d
Nick Orth <nicholas.r.orth@gmail.com>
2022-03-13 19:25:23 -0500

    Add some front end form validation for addresses, values cannot be blank

--- public/js/pubuserupdate.js
+++ public/js/pubuserupdate.js
@@ -1,1 +1,25 @@
 console.log("inside pub user update");
+const updateBtn = document.getElementById("update");
+const vals = document.querySelectorAll(".validate");
+
+updateBtn.addEventListener("click", (e) => {
+  for (let i = 0; i < vals.length; i++) {
+    let val = vals[i];
+    let content = vals[i].value;
+    if (content === "") {
+      e.preventDefault();
+      const label = val.parentElement.previousElementSibling.firstElementChild;
+      label.textContent = `${label.textContent.replace("*", "")}`;
+      label.textContent = `*${label.textContent}`;
+      label.classList.add("red");
+      //   label.textContent = `${label.textContent} *required`;
+      val.classList.add("required");
+      val.addEventListener("click", () => {
+        label.textContent = `${label.textContent.replace("*", "")}`;
+        val.classList.remove("required");
+        label.classList.remove("red");
+      });
+      return;
+    }
+  }
+});
