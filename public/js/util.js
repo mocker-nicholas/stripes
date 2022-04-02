55a0c8252b37b60a17c93c52c222efff2c04f61a
Nick Orth <nicholas.r.orth@gmail.com>
2022-04-02 09:00:37 -0500

    Add navbar transition when clicked

--- public/js/util.js
+++ public/js/util.js
@@ -3,6 +3,16 @@ console.log("inside pub util js");
 ///////////////////////// Display Mobile Menu on a click /////////////////////////////
 const burger = document.querySelector(".burger");
 burger.addEventListener("click", () => {
+  // Get rid of first line
+  const topLine = document.querySelector(".line");
+  topLine.classList.toggle("hide");
+  // move the midde line
+  const midLine = document.querySelector(".line:nth-of-type(2)");
+  midLine.classList.toggle("mid-line-center");
+  // move the last line
+  const lastLine = document.querySelector(".line:nth-of-type(3)");
+  lastLine.classList.toggle("bottom-line-center");
+  // Bring in small navbar
   const smallNav = document.querySelector(".small-nav");
   smallNav.classList.toggle("hide");
 });
